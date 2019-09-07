daaf61835878559fa1b5551933bd6d6e15c778c0
EFLLP <erickllanos30@gmail.com>
2019-09-06 22:28:08 -0500

    *.- Primer componente

---
+++ vue from scratch/components/message.js
@@ -0,0 +1,16 @@
+Vue.component('message', {
+    data() {
+        return {
+            message: 'Hello world'
+                }
+    },
+    template:`
+    <div>
+        <h2> componente message  </h2>
+        {{message}}
+    </div>
+    `
+});
+
+
+
